9eead98192acef894e356b8c5a93a762459a3bb7
matiasercolani <matiaserc514@gmail.com>
2020-12-07 17:26:49 -0300

    Efecto de carga Scroll-Skills

--- js/main.js
+++ js/main.js
@@ -22,13 +22,47 @@ var typed = new Typed('.animate', {
 
 //Carga de partes
 window.addEventListener('scroll', function(){
-    let animacion = document.getElementById('skillsEf');
-    let posicionObj1=animacion.getBoundingClientRect().top;
+    let skHtml = document.getElementById('skHtml');
+    let skCss = document.getElementById('skCss');
+    let skJs = document.getElementById('skJs');
+    let skWord = document.getElementById('skWord');
+    let skPhp = document.getElementById('skPhp');
+    let skSql = document.getElementById('skSql');
+
+
+    let posicionObj1=skSql.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight/3.5;
+    let tamañoDePantalla = window.innerHeight/1;
 
     if(posicionObj1 < tamañoDePantalla){
-        animacion.style.animation = 'mover 1s ease-out'
+        //animacion.style.animation = 'mover 1s ease-out'
+        //HTML
+        skHtml.style.animation = 'skHtml 2s';
+        skHtml.style.width = '90%';
+
+        //CSS
+        skCss.style.animation = 'skCss 2s';
+        skCss.style.width = '85%';
+
+
+        //JS
+        skJs.style.animation = 'skJs 2s';
+        skJs.style.width = '75%';
+
+
+        //WP
+        skWord.style.animation = 'skWord 2s';
+        skWord.style.width = '95%';
+
+        //PHP
+        skPhp.style.animation = 'skPhp 2s';
+        skPhp.style.width = '75%';
+        
+        //SQL
+        skSql.style.animation = 'skSql 2s';
+        skSql.style.width = '85%';
+
+        
     }
 })
 
